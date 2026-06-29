377fbcdb9c5ba62b11ab9572234feb400d492ec9
oscaresteve <oscarestevea24@gmail.com>
2026-06-29 12:41:57 +0200

    feat: anadir boton para eliminar items

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,15 +4,38 @@ import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
-export default async function createItemAction(formData: FormData) {
-  const { userId } = await auth.protect();
+export async function createItemAction(formData: FormData) {
+  const user = await auth.protect();
   const text = formData.get("text") as string;
+  const clerkId = user.userId;
+
+  if (!user) return;
 
   try {
     await prisma.item.create({
       data: {
-        clerkId: userId,
+        clerkId,
         text,
+      },
+    });
+    revalidatePath("/");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteItemAction(formData: FormData) {
+  const user = await auth.protect();
+  const itemId = formData.get("itemId") as string;
+
+  console.log(itemId)
+
+  if (!user) return;
+
+  try {
+    await prisma.item.delete({
+      where: {
+        id: itemId,
       },
     });
     revalidatePath("/");
--- src/components/items-list.tsx
+++ src/components/items-list.tsx
@@ -1,7 +1,7 @@
 "use client";
-import createItemAction from "@/app/actions";
+import { createItemAction, deleteItemAction } from "@/app/actions";
 import { ItemView } from "@/lib/types";
-import { CircleDashed, CircleCheckBig, Plus } from "lucide-react";
+import { CircleDashed, CircleCheckBig, Plus, X } from "lucide-react";
 import Image from "next/image";
 
 type ItemsListProps = {
@@ -23,25 +23,33 @@ export default function ItemsList({ items, avatarMap }: ItemsListProps) {
         const user = avatarMap[item.clerkId];
 
         return (
-          <li key={item.id} className="flex gap-6 items-center">
+          <li key={item.id} className="flex gap-6 items-center py-1 px-3 group">
             <Icon className={`size-5 ${isChecked && "opacity-75"}`} />
             <p className={`text-base ${isChecked && "opacity-75 line-through"}`}>{item.text}</p>
-            {user && (
-              <Image
-                src={user.imageUrl}
-                alt={user.name}
-                width={24}
-                height={24}
-                className="rounded-full ml-auto"
-                title={user.name}
-              />
-            )}
+            <div className="flex gap-4 ml-auto items-center">
+              {user && (
+                <Image
+                  src={user.imageUrl}
+                  alt={user.name}
+                  width={24}
+                  height={24}
+                  className="rounded-full"
+                  title={user.name}
+                />
+              )}
+              <form action={deleteItemAction} className="items-center flex">
+                <input type="hidden" name="itemId" value={item.id} />
+                <button type="submit" className="opacity-40 active:opacity-100 transition-opacity cursor-pointer">
+                  <X className="size-4" />
+                </button>
+              </form>
+            </div>
           </li>
         );
       })}
 
       <li key="add-button">
-        <form action={createItemAction} className="flex gap-6 items-center cursor-pointer">
+        <form action={createItemAction} className="flex gap-6 items-center cursor-pointer py-1 px-3">
           <Plus className="size-5" />
           <input
             type="text"
