909246aa0e527e23d0c8557f24a474ab052ca599
oscaresteve <oscarestevea24@gmail.com>
2026-06-29 13:21:15 +0200

    feat: mostrar los items segun updatedAt

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -2,7 +2,11 @@ import { prisma } from "@/lib/prisma";
 
 export async function readAllItems() {
   try {
-    const result = await prisma.item.findMany();
+    const result = await prisma.item.findMany({
+      orderBy: {
+        updatedAt: "asc",
+      },
+    });
     return result;
   } catch (error) {
     console.error(error);
