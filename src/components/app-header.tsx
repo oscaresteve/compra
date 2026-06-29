42b2cd4ac546e19c684bd7435b48c10a3cf596b7
oscaresteve <oscarestevea24@gmail.com>
2026-06-29 14:00:48 +0200

    style: titulo mas grande

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -5,8 +5,8 @@ export default function AppHeader() {
   return (
     <header className="max-w-xl mx-auto p-4 flex justify-between">
       <div className="flex items-center gap-2">
-        <ShoppingCart className="size-4 text-neutral-400" />
-        <h1 className="text-base font-semibold text-neutral-100">Lista de la compra</h1>
+        <ShoppingCart className="size-5 text-neutral-400 mr-1" />
+        <h1 className="text-xl font-semibold text-neutral-100">Lista de la compra</h1>
       </div>
       <UserButton />
     </header>
